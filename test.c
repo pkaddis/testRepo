502fe295f44a0a3a0b8b37a2de1d3ca5836ceeaf
Phillip Kaddis <33929223+pkaddis@users.noreply.github.com>
2019-12-05 10:29:31 +1100

    remote Change to test.c
    blah blah lbah description

--- test.c
+++ test.c
@@ -2,6 +2,17 @@
 
 
 //This code is the best
+/*
+Steps to Git:
+
+1. Git clone (1 time)
+2. Git status
+3. Git add <filename> (STAGING)
+4. Git commit -m "message" (COMMITED)
+5. Git push (PUSHED)
+
+*/
+
 int main(void)
 {
     printf("Hello World\n");
